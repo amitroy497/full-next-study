dba7cf0d54267e2999352147cfa9c312b879a711
Amit Roy <amitroy497@yahoo.com>
2023-09-26 14:59:20 +0530

    2. Basic Routing with Navigation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,9 @@
+'use client';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const AboutPage = () => {
+	const router = useRouter();
 	return (
 		<>
 			<h1>About Page</h1>
@@ -9,6 +12,14 @@ const AboutPage = () => {
 			<br />
 			<br />
 			<Link href='/'>Go To Home Page</Link>
+			<br />
+			<br />
+			<br />
+			<button onClick={() => router.push('/login')}>
+				Navigate To Login Page
+			</button>
+			<br />
+			<button onClick={() => router.push('/')}>Navigagte To Home Page</button>
 		</>
 	);
 };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,9 @@
+'use client';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const LoginPage = () => {
+	const router = useRouter();
 	return (
 		<>
 			<h1>Login Page</h1>
@@ -9,6 +12,14 @@ const LoginPage = () => {
 			<br />
 			<br />
 			<Link href='/'>Go To Home Page</Link>
+			<br />
+			<br />
+			<br />
+			<button onClick={() => router.push('/about')}>
+				Navigate To About Page
+			</button>
+			<br />
+			<button onClick={() => router.push('/')}>Navigate To Home Page</button>
 		</>
 	);
 };
