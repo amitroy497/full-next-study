52c1ec25c4eefb03dffa423534904988b650f1cf
Amit Roy <amitroy497@yahoo.com>
2023-09-28 16:21:15 +0530

    10. 404 Page Not Found For Particular Segment

---
+++ src/app/about/[...not-found]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutNotFoundPage = () => {
+	return (
+		<>
+			<h1>404 Error</h1>
+			<h2>This about page is not available</h2>
+		</>
+	);
+};
+
+export default AboutNotFoundPage;
