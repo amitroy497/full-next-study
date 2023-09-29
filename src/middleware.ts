7efebb59c28d9cda60c22ab029b0cd05712dc332
Amit Roy <amitroy497@yahoo.com>
2023-09-29 11:43:27 +0530

    12. Using middleware redirect to login page on clicking of only about page, rest all the pages will reflect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server';
 
 export const middleware = (request: any) => {
-	if (request.nextUrl.pathname !== '/login')
-		return NextResponse.redirect(new URL('/login', request.url));
+	// if (request.nextUrl.pathname !== '/login')
+	return NextResponse.redirect(new URL('/login', request.url));
+};
+
+export const config = {
+	matcher: '/about/:path*',
 };
