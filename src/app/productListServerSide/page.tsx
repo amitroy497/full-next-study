6b638258234ef60b646fc78f0453396e1091d591
Amit Roy <amitroy497@yahoo.com>
2023-10-03 12:26:39 +0530

    16. Use Client Component With Server Component

--- src/app/productListServerSide/page.tsx
+++ src/app/productListServerSide/page.tsx
@@ -1,3 +1,4 @@
+import DescriptionClientComponent from './descriptionClientComponent';
 import { fetchProductList } from './fetchProductList';
 
 const ProductListServerSidePage = async () => {
@@ -7,9 +8,12 @@ const ProductListServerSidePage = async () => {
 			<h1>Product List Page</h1>
 			<h2>Fetch Api Data In Server Side</h2>
 			{products.map((product: any) => (
-				<h3 key={product?.id}>
-					id: {product?.id} Name: {product?.title} Price: {product?.price}
-				</h3>
+				<>
+					<h3 key={product?.id}>
+						id: {product?.id} Name: {product?.title} Price: {product?.price}
+					</h3>
+					<DescriptionClientComponent desc={product?.description} />
+				</>
 			))}
 		</>
 	);
