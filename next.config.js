fc245deda6314ef6287a9c9859872c9499f606d7
Amit Roy <amitroy497@yahoo.com>
2023-10-04 19:42:25 +0530

    24. Redirect Feature Using Config For Non Existing Page

--- next.config.js
+++ next.config.js
@@ -16,6 +16,11 @@ const nextConfig = {
 				destination: '/',
 				permanent: false,
 			},
+			{
+				source: '/notExistingPage',
+				destination: '/',
+				permanent: false,
+			},
 		];
 	},
 };
